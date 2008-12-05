88087d20c8706a41a0760a44ea460e2f86d1a1e7
binarylogic <bjohnson@binarylogic.com>
2008-12-05 12:57:34 -0500

    Bump to v1.0.0

--- lib/settingslogic/version.rb
+++ lib/settingslogic/version.rb
@@ -65,8 +65,8 @@ module Settingslogic
       [@major, @minor, @tiny]
     end
 
-    MAJOR = 0
-    MINOR = 9
+    MAJOR = 1
+    MINOR = 0
     TINY  = 0
 
     # The current version as a Version instance
