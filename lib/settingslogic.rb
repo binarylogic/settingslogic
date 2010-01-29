c8da048da83fcca584c6acf41faf74a1a48ded83
Nate Wiger <nate@wiger.org>
2010-01-30 04:28:58 +0800

    def keys as methods rather than using method_missing, added load!/reload!, more docs/specs

--- lib/settingslogic.rb
+++ lib/settingslogic.rb
@@ -3,6 +3,8 @@ require "erb"
 
 # A simple settings solution using a YAML file. See README for more information.
 class Settingslogic < Hash
+  class MissingSetting < StandardError; end
+  
   class << self
     def name # :nodoc:
       instance.key?("name") ? instance.name : super
@@ -15,13 +17,33 @@ class Settingslogic < Hash
         @source = value
       end
     end
-    
+
     def namespace(value = nil)
       if value.nil?
         @namespace
       else
         @namespace = value
       end
+    end
+    
+    def [](key)
+      # Setting.key.value or Setting[:key][:value] or Setting['key']['value']
+      fetch(key.to_s,nil)
+    end
+
+    def []=(key,val)
+      # Setting[:key] = 'value' for dynamic settings
+      store(key.to_s,val)
+    end
+    
+    def load!
+      instance
+      true
+    end
+    
+    def reload!
+      @instance = nil
+      load!
     end
     
     private
@@ -33,7 +55,7 @@ class Settingslogic < Hash
         instance.send(name, *args, &block)
       end
   end
-  
+
   # Initializes a new settings object. You can initialize an object in any of the following ways:
   #
   #   Settings.new(:application) # will look for config/application.yml
@@ -41,38 +63,51 @@ class Settingslogic < Hash
   #   Settings.new("/var/configs/application.yml") # will look for /var/configs/application.yml
   #   Settings.new(:config1 => 1, :config2 => 2)
   #
-  # Basically if you pass a symbol it will look for that file in the configs directory of your rails app, if you are using this in rails. If you pass a string it should be an absolute path to your settings file.
+  # Basically if you pass a symbol it will look for that file in the configs directory of your rails app,
+  # if you are using this in rails. If you pass a string it should be an absolute path to your settings file.
   # Then you can pass a hash, and it just allows you to access the hash via methods.
-  def initialize(hash_or_file = self.class.source)
+  def initialize(hash_or_file = self.class.source, section = nil)
     case hash_or_file
     when Hash
-      self.update hash_or_file
+      self.replace hash_or_file
     else
       hash = YAML.load(ERB.new(File.read(hash_or_file)).result).to_hash
       hash = hash[self.class.namespace] if self.class.namespace
-      self.update hash
+      self.replace hash
     end
+    @section = section || hash_or_file  # so end of error says "in application.yml"
+    create_accessors!
   end
-  
-  module EigenMethodDefiner # :nodoc:
-    def method_missing(name, *args, &block)
-      if key?(name.to_s)
-        define_eigen_method(name.to_s)
-        value = self[name.to_s]
-        value.extend(EigenMethodDefiner) if value.is_a?(Hash)
-        value
-      else
-        super
+
+  # Called for dynamically-defined keys, and also the first key deferenced at the top-level, if load! is not used.
+  # Otherwise, create_accessors! (called by new) will have created actual methods for each key.
+  def method_missing(key, *args, &block)
+    begin
+      value = fetch(key.to_s)
+    rescue KeyError
+      raise MissingSetting, "Missing setting '#{key}' in #{@section}"
+    end
+    value.is_a?(Hash) ? self.class.new(value, "'#{key}' section in #{@section}") : value
+  end
+
+  private
+    # This handles naming collisions with Sinatra/Vlad/Capistrano. Since these use a set()
+    # helper that defines methods in Object, ANY method_missing ANYWHERE picks up the Vlad/Sinatra
+    # settings!  So settings.deploy_to title actually calls Object.deploy_to (from set :deploy_to, "host"),
+    # rather than the app_yml['deploy_to'] hash.  Jeezus.
+    def create_accessors!
+      self.each do |key,val|
+        # Use instance_eval/class_eval because they're actually more efficient than define_method{}
+        # http://stackoverflow.com/questions/185947/ruby-definemethod-vs-def
+        # http://bmorearty.wordpress.com/2009/01/09/fun-with-rubys-instance_eval-and-class_eval/
+        self.class.class_eval <<-EndEval
+          def #{key}
+            return @#{key} if @#{key}  # cache (performance)
+            value = fetch('#{key}')
+            @#{key} = value.is_a?(Hash) ? self.class.new(value, "'#{key}' section in #{@section}") : value
+          end
+        EndEval
       end
     end
 
-    private
-
-      def define_eigen_method(name)
-        eigen_class = class << self; self; end
-        eigen_class.send(:define_method, name) { self[name] }
-      end
-  end
-
-  include EigenMethodDefiner
 end
--- spec/settingslogic_spec.rb
+++ spec/settingslogic_spec.rb
@@ -1,10 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
 
 describe "Settingslogic" do
-  it "should be a hash" do
-    Settings.send(:instance).should be_is_a(Hash)
-  end
-  
   it "should access settings" do
     Settings.setting2.should == 5
   end
@@ -16,21 +12,78 @@ describe "Settingslogic" do
   it "should access deep nested settings" do
     Settings.setting1.deep.another.should == "my value"
   end
-  
+
   it "should access extra deep nested settings" do
     Settings.setting1.deep.child.value.should == 2
   end
-  
+
   it "should enable erb" do
     Settings.setting3.should == 25
   end
   
   it "should namespace settings" do
     Settings2.setting1_child.should == "saweet"
+    Settings2.deep.another.should == "my value"
+  end
+
+  it "should return the namespace" do
+    Settings.namespace.should be_nil
+    Settings2.namespace.should == 'setting1'
   end
 
   it "should distinguish nested keys" do
     Settings.language.haskell.paradigm.should == 'functional'
     Settings.language.smalltalk.paradigm.should == 'object oriented'
+  end
+  
+  it "should not collide with global methods" do
+    Settings3.collides.does.should == 'not'
+  end
+  
+  it "should raise a helpful error message" do
+    e = nil
+    begin
+      Settings.missing
+    rescue => e
+      e.should be_kind_of Settingslogic::MissingSetting
+    end
+    e.should_not be_nil
+    e.message.should =~ /Missing setting 'missing' in/
+    
+    e = nil
+    begin
+      Settings.language.missing
+    rescue => e
+      e.should be_kind_of Settingslogic::MissingSetting
+    end
+    e.should_not be_nil
+    e.message.should =~ /Missing setting 'missing' in 'language' section/
+  end
+
+  it "should handle optional / dynamic settings" do
+    e = nil
+    begin
+      Settings.language.erlang
+    rescue => e
+      e.should be_kind_of Settingslogic::MissingSetting
+    end
+    e.should_not be_nil
+    e.message.should =~ /Missing setting 'erlang' in 'language' section/
+    
+    Settings.language['erlang'].should be_nil
+    Settings.language['erlang'] ||= 5
+    Settings.language['erlang'].should == 5
+
+    Settings.language['erlang'] = {'paradigm' => 'functional'}
+    Settings.language.erlang.paradigm.should == 'functional'
+
+    Settings.reload!
+    Settings.language['erlang'].should be_nil
+  end
+
+  # Put this test last or else call to .instance will load @instance,
+  # masking bugs.
+  it "should be a hash" do
+    Settings.send(:instance).should be_is_a(Hash)
   end
 end
